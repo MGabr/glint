5e7031338c39083cf5a4a042fda5901a546dfae9
Markus Gabriel <e1326657@student.tuwien.ac.at>
2019-02-03 12:47:34 +0100

    Use correct executor-cores value from config

--- src/main/scala/glint/Client.scala
+++ src/main/scala/glint/Client.scala
@@ -373,14 +373,14 @@ object Client {
       val client = Client(config)
 
       // Start partition master
-      val nrOfExecutors = sc.getExecutorMemoryStatus.size - 1
+      val nrOfExecutors = sc.getConf.get("spark.executor.instances").toInt
       val numParameterServers = nrOfExecutors
       val partitioner = RangePartitioner(numParameterServers, numParameterServers)
       val (partitionMasterSystem, partitionMasterActor) = Await.result(PartitionMaster.run(config, partitioner.all()),
         clientTimeout)
 
       // Start parameter servers on workers
-      val executorCores = Runtime.getRuntime.availableProcessors
+      val executorCores = sc.getConf.get("spark.executor.cores").toInt
       val nrOfPartitions = nrOfExecutors * executorCores
       sc.range(0, nrOfPartitions, numSlices = nrOfPartitions).foreachPartition {
         case _ => Await.result(Server.runOnce(config), clientTimeout)
@@ -507,8 +507,8 @@ object Client {
 
       // Start parameter servers and create 'numParameterServers' partial models on workers
       // Return list of serialized partial model actor references
-      val nrOfExecutors = sc.getExecutorMemoryStatus.size - 1
-      val executorCores = Runtime.getRuntime.availableProcessors
+      val nrOfExecutors = sc.getConf.get("spark.executor.instances").toInt
+      val executorCores = sc.getConf.get("spark.executor.cores").toInt
       val nrOfPartitions = nrOfExecutors * executorCores
       val models = sc.range(0, nrOfPartitions, numSlices = nrOfPartitions).mapPartitions {
         iter =>
